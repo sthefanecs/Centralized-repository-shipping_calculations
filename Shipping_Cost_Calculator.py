94aeb09815bd5c7f9d83a445b9e0a51271500137
sthefanecs <sthefanecosta2101@gmail.com>
2025-11-05 19:57:40 -0300

    Add placeholder for shipping cost calculation

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -7,6 +7,8 @@ rate = float(input("Enter the shipping rate per kilogram: "))
 ## Calculate shipping cost
 shipping_cost = weight * rate
 
+###
+
 
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
